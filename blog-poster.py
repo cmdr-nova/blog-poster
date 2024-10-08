a5da73ecda0d85928b84ad3fa78abf184ac22cea
cmdr-nova <96963723+cmdr-nova@users.noreply.github.com>
2024-10-08 10:32:04 -0400

    Update blog-poster.py

--- blog-poster.py
+++ blog-poster.py
@@ -1,19 +1,28 @@
 import feedparser
 import json
 import os
+import requests
 from mastodon import Mastodon
 
 # constants
-RSS_FEED_URL = 'your_feed_url'
+RSS_FEED_URL = 'your_blog_rss_feed'
 POSTED_ITEMS_FILE = 'posted_items.json'
-INSTANCE_URL = 'your_mastodon_url'
+INSTANCE_URL = 'your_mastodon_instance_url'
 ACCESS_TOKEN = 'your_mastodon_access_token'
+DISCORD_WEBHOOK_URL = 'your_discord_webhook'
 
 # initialize Mastodon client
 mastodon = Mastodon(
     access_token=ACCESS_TOKEN,
     api_base_url=INSTANCE_URL
 )
+
+def share_to_discord(title, link, excerpt):
+    data = {
+        'content': f"{title}\n{link}\n\n{excerpt}"
+    }
+    response = requests.post(DISCORD_WEBHOOK_URL, json=data)
+    response.raise_for_status()
 
 # load the list of already posted items
 if os.path.exists(POSTED_ITEMS_FILE):
@@ -33,12 +42,19 @@ feed = feedparser.parse(RSS_FEED_URL)
 # check for new items in the RSS feed
 new_items = [entry for entry in feed.entries if entry.id not in posted_items]
 
-# post new items to Mastodon
+# post new items to Mastodon and Discord
 for item in new_items:
-    custom_text = "This is a line for any custom text you want to have shared along with the blog title, link, and excerpt."
+    custom_text = "Put your own text in here that shares with each blog post!"
     excerpt = item.summary if 'summary' in item else 'No excerpt available'
     status_message = f"{custom_text}\n{item.title}\n{item.link}\n\n{excerpt}"
+    
+    # post to Mastodon
     mastodon.status_post(status=status_message)
+    
+    # post to Discord
+    share_to_discord(item.title, item.link, excerpt)
+    
+    # add item ID to posted items
     posted_items.append(item.id)
 
 # update the list of posted items and save it back to the file
